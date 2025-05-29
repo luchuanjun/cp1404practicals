db7744ef96d120cc44cd966e9459f0ef643336f7
chuanjunlu <chuanjun.lu@my.jcu.edu.au>
2025-05-29 13:30:22 +0800

    Add prac_02 files

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,7 +2,7 @@ def main():
     password = get_password()
     print_asterisks(password)
 
-# 获取用户输入密码的函数
+
 def get_password():
     password = input("请输入密码（Enter password）: ")
     while len(password) < 5:
@@ -10,12 +10,12 @@ def get_password():
         password = input("请输入密码（Enter password）: ")
     return password
 
-# 打印星号的函数，可选参数symbol默认为 *
+
 def print_asterisks(password, symbol='*'):
     print(symbol * len(password))
 
 
-# 主函数入口
+
 if __name__ == '__main__':
     main()
 
