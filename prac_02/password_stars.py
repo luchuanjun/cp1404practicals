3085ece583a5a4d07d96941ec6e396fc886e2217
chuanjunlu <chuanjun.lu@my.jcu.edu.au>
2025-05-29 13:32:00 +0800

    Add prac_02 files

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -4,10 +4,10 @@ def main():
 
 
 def get_password():
-    password = input("请输入密码（Enter password）: ")
+    password = input("Enter password: ")
     while len(password) < 5:
-        print("密码太短！请输入至少5个字符。（Password too short! Please enter at least 5 characters.）")
-        password = input("请输入密码（Enter password）: ")
+        print("Password too short! Please enter at least 5 characters.")
+        password = input("Enter password）: ")
     return password
 
 
