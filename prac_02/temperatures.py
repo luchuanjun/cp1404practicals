93df88bec0d2734301528199561a5dc9dc8f666e
chuanjunlu <chuanjun.lu@my.jcu.edu.au>
2025-05-29 13:44:42 +0800

    Add prac_02 files

---
+++ prac_02/temperatures.py
@@ -0,0 +1,30 @@
+def main():
+    MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ - Quit"
+    print(MENU)
+    choice = input(">>> ").upper()
+
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Enter temperature in Celsius: "))
+            fahrenheit = celsius_to_fahrenheit(celsius)
+            print(f"{celsius:.1f}°C is {fahrenheit:.1f}°F")
+        elif choice == "F":
+            fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+            celsius = fahrenheit_to_celsius(fahrenheit)
+            print(f"{fahrenheit:.1f}°F is {celsius:.1f}°C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+
+    print("Thank you.")
+
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9.0 / 5 + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+
+if __name__ == "__main__":
+    main()
