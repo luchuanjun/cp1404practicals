90be7d2ba4aea91b46b562514f89b9007f02534c
chuanjunlu <chuanjun.lu@my.jcu.edu.au>
2025-06-05 14:17:07 +0800

    Add prac3 files

---
+++ prac_03/files.py
@@ -0,0 +1,20 @@
+name = input("Enter your name: ")
+name_file = open('name.txt', 'w')
+name_file.write(name)
+name_file.close()
+
+name_file = open('name.txt', 'r')
+stored_name = name_file.read()
+name_file.close()
+print(f"Hi {stored_name}!")
+
+with open('numbers.txt', 'r') as file:
+    first = int(file.readline())
+    second = int(file.readline())
+print(first + second)  # 应该是59
+
+total = 0
+with open('numbers.txt', 'r') as file:
+    for line in file:
+        total += int(line.strip())
+print(total)
