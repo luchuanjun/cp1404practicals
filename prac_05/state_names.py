ce21328a7c1bf18bd0ba85cb821cb940da49054d
chuanjunlu <chuanjun.lu@my.jcu.edu.au>
2025-06-17 11:27:20 +0800

    Reformat state_names.py and add case-insensitive support

---
+++ prac_05/state_names.py
@@ -0,0 +1,28 @@
+"""
+State names dictionary
+Estimate: 20 minutes
+Actual:   25 minutes
+"""
+
+CODE_TO_NAME = {
+    "QLD": "Queensland",
+    "NSW": "New South Wales",
+    "NT": "Northern Territory",
+    "WA": "Western Australia",
+    "ACT": "Australian Capital Territory",
+    "VIC": "Victoria",
+    "TAS": "Tasmania",
+    "SA": "South Australia"
+}
+
+print("All states:")
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:3} is {name}")
+
+state_code = input("\nEnter short state: ").upper()
+while state_code != "":
+    try:
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
+        print("Invalid short state")
+    state_code = input("Enter short state: ").upper()
